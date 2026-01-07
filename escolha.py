f7568ae049984f42288007c22a48296e6bb44fc8
Gabrielle Mota <gxbyelle@gmail.com>
2026-01-07 17:08:43 -0300

    Create escolha.py

---
+++ escolha.py
@@ -0,0 +1,6 @@
+n1 = float(input("Escolha o primeiro número: "))
+n2 = float(input("Escolha o segundo número: "))
+n3 = float(input("Escolha o terceiro número: "))
+n1 += n2 + n3
+n1 /= 3
+print(f"A média dos três números é: {n1:.2f}") 
