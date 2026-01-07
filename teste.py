7eeec0b61bc1966d5a8f2e57a569939c6bf817bc
Gabrielle Mota <gxbyelle@gmail.com>
2026-01-07 16:41:12 -0300

    Create teste.py

---
+++ teste.py
@@ -0,0 +1,15 @@
+n1 = float(input("Digite um número: "))
+n2 = float(input("Digite outro número: "))
+soma = n1 + n2
+multiplicacao = n1 * n2
+subtracao = n1 - n2
+divisao = n1 / n2
+quociente = int(n1 / n2)
+resto_da_divisao = n1 % n2
+
+print(f"{n1} + {n2} = {soma}")
+print(f"{n1} - {n2} = {subtracao}")
+print(f"{n1} * {n2} = {multiplicacao}")
+print(f"{n1} / {n2} = {divisao}")
+print(f"Quociente da divisão = {quociente}")
+print(f"Resto da divisão = {resto_da_divisao}")
