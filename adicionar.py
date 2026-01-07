73b1bd43cb216b729d01f5c400ebc9444b0e07bc
Gabrielle Mota <gxbyelle@gmail.com>
2026-01-07 17:01:07 -0300

    Update adicionar.py

--- adicionar.py
+++ adicionar.py
@@ -1,5 +1,9 @@
 n1 = int(input("Escolha um número: "))
 n1 += 1
-print(f"Incrementando 1 ao número escolhido fica: {n1}")
+print(f"Adicionando 1 ao número escolhido fica: {n1}")
 n1 -= 1
-print(f"Decrementando 1 ao número escolhido fica: {n1}")
+print(f"Subtraindo 1 ao número fica: {n1}")
+n1 *= 2
+print(f"Multiplicando o número por 2 fica: {n1}")
+n1 /= 2
+print(f"Divididindo o número na metade fica: {n1}")
