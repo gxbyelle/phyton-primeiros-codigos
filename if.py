7f8488d266ab552a8b831ed7bd317aa69f35139d
Gabrielle Mota <gxbyelle@gmail.com>
2026-01-07 21:37:43 -0300

    Create if.py

---
+++ if.py
@@ -0,0 +1,7 @@
+nota = float(input("Insire sua nota: "))
+if nota >= 7.0:
+    print("Aprovado(a)! Parabéns!")
+if nota < 7.0 and nota >= 4.0:
+    print("Faça a recuperação")
+if nota < 4.0:
+    print("Reprovado(a)")
