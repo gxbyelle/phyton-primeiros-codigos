97a0a5f233c0f3c51566e0f7e6696ade6bb4db4a
Gabrielle Mota <gxbyelle@gmail.com>
2026-01-07 22:23:15 -0300

    Create mes-dia.py

---
+++ mes-dia.py
@@ -0,0 +1,49 @@
+# Semanas
+dia = int(input("\nDigite um número inteiro de 1 a 7: "))
+if dia == 1:
+    print("Domingo")
+elif dia == 2:
+    print("Segunda-Feira")
+elif dia == 3:
+    print("Terça-Feira")
+elif dia == 4:
+    print("Quarta-Feira")
+elif dia == 5:
+    print("Quinta-Feira")
+elif dia == 6:
+    print("Sexta-Feira")
+elif dia == 7:
+    print("Sábado")
+else:
+    print("Número inválido.")
+
+# Meses
+numero = int(input("\nDigite um número inteiro de 1 a 12: "))
+if numero == 1:
+    print("Janeiro")
+elif numero == 2:
+    print("Fevereiro")
+elif numero == 3:
+    print("Março")
+elif numero == 4:
+    print("Abril")
+elif numero == 5:
+    print("Maio")
+elif numero == 6:
+    print("Junho")
+elif numero == 7:
+    print("Julho")
+elif numero == 8:
+    print("Agosto")
+elif numero == 9:
+    print("Setembro")
+elif numero == 10:
+    print("Outubro")
+elif numero == 11:
+    print("Novembro")
+elif numero == 12:
+    print("Dezembro")
+else:
+    print("Número inválido.")
+
+print("\nObrigada por participar do teste! :) S2")
