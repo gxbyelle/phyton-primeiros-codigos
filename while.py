85d363c40b69a327943415b54c7d455ecaa38e2c
Gabrielle Mota <gxbyelle@gmail.com>
2026-01-08 21:40:33 -0300

    Update while.py

--- while.py
+++ while.py
@@ -1,7 +1,9 @@
 inteiro = int(input("Digite um número inteiro: "))
 soma = 0
+contador = 0
 while inteiro > 0:
     if inteiro % 5 != 0:
         soma += inteiro
+        contador += 1
     inteiro = int(input("Digite um número inteiro: "))
-print(f"A soma dos números positivos é: {soma}")
+print(f"A soma dos números positivos é: {soma}\nForam digitados {contador} números positivos não multiplos de 5.")
